ea4b687e1cf7f1ffd00593b0727e93654526217b
chulakasam <sampathchulaka33872@gmail.com>
2025-05-26 12:03:13 +0530

    create product  entity

--- src/main/java/com/ceyentra/online_store_REST_API/entities/Product.java
+++ src/main/java/com/ceyentra/online_store_REST_API/entities/Product.java
@@ -1,4 +1,33 @@
 package com.ceyentra.online_store_REST_API.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
 }
