611db90fcbe810d36acb05a44e93f4c9bb7a31e8
chulakasam <sampathchulaka33872@gmail.com>
2025-05-26 13:02:31 +0530

    implement home controller and create message entity ,create message controller

--- src/main/java/com/ceyentra/online_store_REST_API/controller/HomeController.java
+++ src/main/java/com/ceyentra/online_store_REST_API/controller/HomeController.java
@@ -1,8 +1,16 @@
 package com.ceyentra.online_store_REST_API.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("name", "sampath");
+        return "index";
+    }
 
 }
---
+++ src/main/java/com/ceyentra/online_store_REST_API/entities/Message.java
@@ -0,0 +1,12 @@
+package com.ceyentra.online_store_REST_API.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Generated;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Message {
+    private String text;
+
+}
